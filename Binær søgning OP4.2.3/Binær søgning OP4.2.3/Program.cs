94c9a320cc55fa21ef788340e6967fffeb38e483
NiceCreeper3 <124564568+NiceCreeper3@users.noreply.github.com>
2023-03-29 09:43:22 +0200

    Opgave4.2.3OPgaveSamling

---
+++ Binær søgning OP4.2.3/Binær søgning OP4.2.3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Binær_søgning_OP4._2._3
+{
+    class Program
+    {
+        // arrey med random nummrer
+        static int[] LomsortOfArrey = new int[] {6,2,9,14,6,1,5};
+
+        static void Main(string[] args)
+        {
+
+            //rites the finishd array
+            foreach(int n in LomsortOfArrey)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
